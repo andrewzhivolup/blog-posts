fdeaebba37237b8f83000973e382e250c90e478e
andrewzhivolup <ferari1190@mail.ru>
2022-04-19 00:45:18 +0300

    add counter, postItem, postlist

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import React, { useState } from "react";
+import Counter from "./components/Counter";
+import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
+import "./styles/App.css"
 
 function App() {
+
+const [posts, setPosts] = useState([
+  {id:1, title: 'JS', body: 'js - yazik'},
+  {id:2, title: 'JSx', body: 'js - yazik'},
+  {id:3, title: 'JSc', body: 'js - yazik'}
+])
+
   return (
     <div className="App">
-      
+      <PostList posts = {posts} title = {'Список постов'}/>
     </div>
   );
 }
