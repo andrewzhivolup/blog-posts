4333d52eb6248a9cec2779f2beb379a4aeda90dd
andrewzhivolup <ferari1190@mail.ru>
2022-04-21 15:27:49 +0300

    add form

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import Counter from "./components/Counter";
+import PostForm from "./components/PostForm";
 import PostItem from "./components/PostItem";
 import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
 import "./styles/App.css"
 
 function App() {
 
-const [posts, setPosts] = useState([
-  {id:1, title: 'JS', body: 'js - yazik'},
-  {id:2, title: 'JSx', body: 'js - yazik'},
-  {id:3, title: 'JSc', body: 'js - yazik'}
-])
+  const [posts, setPosts] = useState([
+    { id: 1, title: 'JavaScript', body: 'js - yazik' },
+    { id: 2, title: 'Python', body: 'Python - yazik' },
+    { id: 3, title: 'C++', body: 'C++ - yazik' }
+  ])
+
+  
+
+  const addNewPost = (e) => {
+    e.preventDefault()
+      setPosts([...posts, {...post, id:Date.now()}])
+      setPost({title: '', body: ''})
+  }
 
   return (
     <div className="App">
-      <PostList posts = {posts} title = {'Список постов'}/>
+      <PostForm></PostForm>
+      <PostList posts={posts} title={'Список постов'} />
     </div>
   );
 }
